1bcb0202414fc6debcd7fa0f3ffeea26c33b67bf
unknown <duaa@example.com>
2024-10-05 13:34:18 +0100

    Add Header component with mni component

---
+++ src/Mui-component/Header.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { AppBar, Avatar, Box, Button, Container, IconButton, Link, Menu, MenuItem, Toolbar, Tooltip, Typography } from '@mui/material'
+import Logo from "../assets/Logo.png"
+
+const Header = () => {
+    return (
+        <div className='bg-light'>
+            <AppBar position="static" sx={{ backgroundColor: "#f5f5f5" }}>
+                <Container maxWidth="xl">
+                    <Toolbar sx={{display:"flex", justifyContent:"space-between"}}>
+                        <Box>
+                            <img src={Logo} alt="logo" srcset="" />
+                        </Box>
+
+                        <Box sx={{display:"flex",}}>
+                            <Link href="#" underline="none"  className='mx-3 text-dark'>
+                                <Typography variant='body1'>Home</Typography>
+                            </Link>
+                            <Link href="#" underline="none"  className='mx-3 text-dark'>
+                                <Typography variant='body1'>Service</Typography>
+                            </Link>
+                            <Link href="#" underline="none"  className='mx-3 text-dark'>
+                                <Typography variant='body1'>Feature</Typography>
+                            </Link>
+                            <Link href="#" underline="none"  className='mx-3 text-dark'>
+                                <Typography variant='body1'>Product</Typography>
+                            </Link>
+                            <Link href="#" underline="none"  className='mx-3 text-dark'>
+                                <Typography variant='body1'>Testimonial</Typography>
+                            </Link>
+                            <Link href="#" underline="none"  className='mx-3 text-dark'>
+                                <Typography variant='body1'>faQ</Typography>
+                            </Link>
+                            
+                        </Box>
+                        <Box>
+                        <Button color="success">Login</Button>
+                        <Button variant="contained" color="success">Signup</Button>
+                        </Box>
+
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </div>
+    )
+}
+
+export default Header
